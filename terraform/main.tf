d8100bfc7e12899581f00f67ac6fae3cb2f5b51c
Vadim Morozov <102137707+noshavenovemba@users.noreply.github.com>
2022-12-06 19:50:28 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+}
+
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
@@ -11,3 +20,12 @@ resource "aws_instance" "ec2_instance" {
     instance_type = "${var.instance_type}"
     key_name = "${var.ami_key_pair_name}"
 } 
+
+resource "aws_instance" "demo" {
+ ami = “ami-00831fc7c1e3ddc60”
+ instance_type = “t2.micro”
+
+ tags = {
+   name = "Demo System"
+ }
+}
